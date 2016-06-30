96c7a58784c1372d6642226547351b4af5280e4a
Spencer Cheng <spenccheng@gmail.com>
2016-06-30 15:46:49 +0000

    final

--- Drummer.py
+++ Drummer.py
@@ -23,23 +23,45 @@ class Guitarist(Musician):
         
 class Drummer(Musician):
     def __init__(self):
-        super().__init__(["Bang", "Bong"]).solo()
-        
-    def count(self, duration):
-        for i in range(duration):
-            print(i)
+        super().__init__(["Bang", "Bong"])
+
+    def count(self):
+        for i in range(4):
+            print(i+1)
     
     def combust(self):
         print("Kaboom")
-class Band(object):
-    def __init__(self, members):
-        self.members = members
+class Band(list):
+    def __init__(self, performers=()):
+        self.members = list(performers)
+       
+    def hire(self, person):
+        self.members.append(person)
+        print("Hired Person")
+    def fire(self,person):
+        self.members.remove(person)
+        print("Fired someone")
+    def play(self):
+        for x in self.members:
+            try:
+                x.count()
+                x.solo(4)
+            except:
+                x.solo(4)
+            
+            
         
-    def hire(Musician):
-        members.append(Musician)
+if __name__ == '__main__':
+    d= Drummer()
+    g= Guitarist()
+    ba = Bassist()
+    b = Band()
+    b.hire(d)
+    b.hire(g)
+    b.play()
+    b.hire(ba)
+    b.play()
     
-    def fire(Musician):
-        members.remove(Musician)
     
 
         
